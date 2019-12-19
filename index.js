d7dc505b66a4a624e6560fe76728049f17a11c12
manugroot <manishreddy9963@gmail.com>
2019-12-19 11:17:03 +0530

    Created web server

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("hey manish");
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => res.send('Hello Manish!'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
