90e23df5be435d69780d22d4d2ffc8200c00649e
manugroot <manishreddy9963@gmail.com>
2019-12-21 22:23:47 +0530

    basic webpage

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
+
 const express = require('express')
 const app = express()
 const port = process.env.PORT||3000
 app.use(express.static('public'))
-app.get('/', (req, res) => res.sendFile(__dirname+'/public/htmls/index.html'))
+
+app.get('/', (req, res) => res.sendFile(__dirname + '/public/htmls/index.html'))
 
 var obj={}
-obj.name="R.Manish Reddy";
-obj.regno="17h51a04g6";
-obj.clg="CMRCET";
+obj.name = "R.Manish Reddy";
+obj.clg = "CMRCET";
+obj.rengno = "17h51a04g6";
 
-app.get('/data',(req,res) => res.json(obj))
+app.get('/data', (req, res) => res.json(obj))
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+
+
+
+
+
+
--- public/js/sites.js
+++ public/js/sites.js
@@ -1,8 +1,8 @@
-$(document).ready(function()){
-$.getJSON("/data",function(d){
-
+$(document).ready(function(){
+	$.getJSON("/data",function(d){
+	
 	$("#name").text(d.name);
-	$("#regno").text(d.regno);
 	$("#clg").text(d.clg);
+	$("#regno").text(d.regno);
 	})
 })
